6c3304e0966d6689d32264971dcd5d1fbf8e395e
Lauren Hess <laurenbethhess@gmail.com>
2022-02-16 21:45:43 -0500

    made is user specific

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,8 +4,7 @@ import Typography from '@mui/material/Typography';
 import NewTodo from './NewTodo';
 
 function Home({ user, todos, updateTodo, onTodoDelete, onAddTodo }) {
-
-    const renderTodosListAll = todos.map(todo => <TodoCard updateTodo={updateTodo} todo={todo} key={todo.id} updateTodo={updateTodo}/>)
+    const renderTodosListAll = todos.map(todo => <TodoCard updateTodo={updateTodo} todo={todo} key={todo.id} updateTodo={updateTodo} onTodoDelete={onTodoDelete}/>)
 
     return (
         <div align='center' style={{ paddingTop: 100}}>
